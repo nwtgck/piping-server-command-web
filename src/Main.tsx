5114e43dff55de28747ee38caaff4200a0852566
Ryo Ota <nwtgck@nwtgck.org>
2021-05-27 22:44:49 +0900

    show tooltip when command copied

--- src/Main.tsx
+++ src/Main.tsx
@@ -6,7 +6,7 @@ import {
   InputLabel,
   OutlinedInput,
   Paper,
-  TextField,
+  TextField, Tooltip,
   Typography
 } from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search';
@@ -19,8 +19,17 @@ const paperStyle = {padding: '1rem', marginBottom: '1.5rem'};
 const textFieldStyle = {marginBottom: '1.5rem'};
 
 function TextFieldWithCopy(props: {label: string, value: string, rows: number, style?: React.CSSProperties}) {
-  // (base: https://material-ui.com/components/text-fields/#input-adornments)
+  const [copyTooltipOpen, setCopyTooltipOpen] = React.useState(false);
+
+  const copyButtonClicked = async () => {
+    setCopyTooltipOpen(true);
+    await clipboardCopy(props.value);
+    await new Promise(resolve => setTimeout(resolve, 1500));
+    setCopyTooltipOpen(false);
+  };
+
   return (
+    // (base: https://material-ui.com/components/text-fields/#input-adornments)
     <FormControl variant="outlined" style={props.style} fullWidth>
       <InputLabel>{props.label}</InputLabel>
       <OutlinedInput
@@ -30,12 +39,21 @@ function TextFieldWithCopy(props: {label: string, value: string, rows: number, s
         rows={props.rows}
         endAdornment={
           <InputAdornment position="end">
-            <IconButton
-              aria-label="copy text to the clipboard"
-              onClick={() => clipboardCopy(props.value)}
-              edge="end">
-              <FileCopyIcon />
-            </IconButton>
+            {/* base: https://material-ui.com/components/tooltips/#triggers */}
+            <Tooltip
+              PopperProps={{disablePortal: true}}
+              open={copyTooltipOpen}
+              disableFocusListener
+              disableHoverListener
+              disableTouchListener
+              title="Copied">
+              <IconButton
+                aria-label="copy text to the clipboard"
+                onClick={copyButtonClicked}
+                edge="end">
+                <FileCopyIcon />
+              </IconButton>
+            </Tooltip>
           </InputAdornment>
         }
         labelWidth={70}
