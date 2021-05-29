d0919652a849ee19e4ad1fb7aed959f8f9884a82
Ryo Ota <nwtgck@nwtgck.org>
2021-05-29 18:27:57 +0900

    theme improvement

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,37 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import {Typography} from '@material-ui/core';
+import {Typography, useMediaQuery} from '@material-ui/core';
 import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
 import CssBaseline from '@material-ui/core/CssBaseline';
 import {Main} from "./Main";
-
-const theme = createMuiTheme({
-  palette: {
-    type: window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : undefined,
-  },
-});
+import {indigo, pink} from "@material-ui/core/colors";
 
 function App() {
+  // (base: https://github.com/mui-org/material-ui/blob/25acfab1471080223e308cb0d337dae3832f740f/docs/src/pages/customization/palette/palette.md#system-preference)
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+  const theme = React.useMemo(() => {
+    const primary = indigo;
+    const secondary = pink;
+    // (from: https://github.com/mui-org/material-ui/issues/18776#issuecomment-564570288)
+    const darkPrimary = {
+      light: primary[100],
+      main: primary[200],
+      dark: primary[300],
+    };
+    const darkSecondary = {
+      light: secondary[100],
+      main: secondary[200],
+      dark: secondary[300],
+    };
+    return createMuiTheme({
+      palette: {
+        type: prefersDarkMode ? 'dark' : undefined,
+        primary: prefersDarkMode ? darkPrimary : undefined,
+        secondary: prefersDarkMode ? darkSecondary : undefined,
+      },
+    })
+  }, [prefersDarkMode]);
+
   return (
     <>
       <ThemeProvider theme={theme}>
