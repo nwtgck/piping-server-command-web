72c24f9f2e8b406505e7a59e38b30af2c4bd81a7
Ryo Ota <nwtgck@nwtgck.org>
2021-06-03 23:27:58 +0900

    gpg E2EE for file transfer

--- src/command-componets/file-transfer.tsx
+++ src/command-componets/file-transfer.tsx
@@ -10,7 +10,8 @@ export const fileTransfer = {
   title: 'File transfer',
   searchTags: [],
   component: ({pipingServerUrl, randomString}: {pipingServerUrl: string, randomString: string}) => {
-    const [e2ee, setE2ee] = useState<'none' | 'openssl'>('none');
+    const [textFieldRows, setTextFieldRows] = useState(1);
+    const [e2ee, setE2ee] = useState<'none' | 'openssl' | 'gpg'>('none');
     const [opensslCipherAlgorithm, setOpensslCipherAlgorithm] = useState('aes-256-cbc');
     const url = urlJoin(pipingServerUrl, `myfile${randomString}`);
     const filePath = `myfile`;
@@ -20,6 +21,8 @@ export const fileTransfer = {
           return `curl -T ${filePath} ${url}`;
         case "openssl":
           return `cat ${filePath} | openssl ${opensslCipherAlgorithm} -pbkdf2 | curl -T - ${url}`;
+        case "gpg":
+          return `export GPG_TTY=$(tty);\ncat ${filePath} | gpg -c | curl -T - ${url}`;
       }
     })();
     const receiverCommand = (() => {
@@ -28,6 +31,8 @@ export const fileTransfer = {
           return `curl ${url} > ${filePath}`;
         case "openssl":
           return `curl -sS ${url} | openssl ${opensslCipherAlgorithm} -d -pbkdf2`;
+        case "gpg":
+          return `export GPG_TTY=$(tty);\ncurl -sS ${url} | gpg -d`;
       }
     })();
 
@@ -37,25 +42,29 @@ export const fileTransfer = {
           <TextFieldWithCopy
             label="Sender"
             value={senderCommand}
-            rows={1}
+            rows={textFieldRows}
           />
         </Grid>
         <Grid item xs={12}>
           <TextFieldWithCopy
             label="Receiver"
             value={receiverCommand}
-            rows={1}
+            rows={textFieldRows}
           />
         </Grid>
         <Grid item xs={12}>
           <RadioInput
             label="E2E encryption"
             value={e2ee}
-            onChange={setE2ee}
+            onChange={(e2ee) => {
+              setTextFieldRows(e2ee === "gpg" ? 2 : 1);
+              setE2ee(e2ee);
+            }}
             selections={
               [
                 { label: 'none', value: 'none' },
                 { label: 'openssl', value: 'openssl' },
+                { label: 'gpg', value: 'gpg' },
               ]
             }
           />
